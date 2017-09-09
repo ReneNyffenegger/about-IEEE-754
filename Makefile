87ae4df7522772edf1a88b008c4f7724e4371761
René Nyffenegger <mail@renenyffenegger.ch>
2017-09-09 05:31:55 +0200

    + nextafter

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
 
-all: print-some-doubles min-double
+all: print-some-doubles min-double nextafter
 
 print-some-doubles: print-some-doubles.o represent-ieee-754.o
 	gcc -Wall -Wextra print-some-doubles.o represent-ieee-754.o -o print-some-doubles
 
 min-double: min-double.o represent-ieee-754.o
 	gcc -Wall -Wextra min-double.o represent-ieee-754.o -o min-double
+
+nextafter: nextafter.o represent-ieee-754.o
+	gcc -Wall -Wextra nextafter.o represent-ieee-754.o -o nextafter -lm
 
 print-some-doubles.o: print-some-doubles.c represent-ieee-754.h
 	gcc -Wall -Wextra -c print-some-doubles.c -o print-some-doubles.o
@@ -15,4 +18,7 @@ min-double.o: min-double.c represent-ieee-754.h
 
 represent-ieee-754.o: represent-ieee-754.c represent-ieee-754.h
 	gcc -Wall -Wextra -c represent-ieee-754.c -o represent-ieee-754.o
+
+nextafter.o: nextafter.c represent-ieee-754.h
+	gcc -Wall -Wextra -c nextafter.c -o nextafter.o
 
---
+++ nextafter.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <math.h>
+
+#include "represent-ieee-754.h"
+
+int main() {
+
+   ieee_754_double_representation r;
+
+   double d = 0; represent_ieee_754_double(d, r);
+   
+   printf("\nWe start with the representation of the double 0:\n");
+   printf("    %s\n", r);
+
+   d=nextafter(d, INFINITY); represent_ieee_754_double(d, r);
+   printf("\nThe next higher representable double just increases the value of the");
+   printf("\nmantissa by one:\n");
+   printf("    %s\n", r);
+
+   d=nextafter(d, INFINITY); represent_ieee_754_double(d, r);
+   printf("\nEach call to nextafter() increases the value of the mantissa by one:\n");
+   printf("    %s\n", r);
+
+   d=nextafter(d, INFINITY); represent_ieee_754_double(d, r);
+   printf("\nAgain...:\n");
+   printf("    %s\n", r);
+
+   d=nextafter(1.0/pow(2, 1022), -INFINITY);
+   printf("\nThe mantissa increasing can be continued until all bits of the");
+   printf("\nmantissa are set:\n");
+   represent_ieee_754_double(d, r); printf("    %s\n", r);
+
+   d=nextafter(d, INFINITY); represent_ieee_754_double(d, r);
+   printf("\nNow, increasing the double to the next representable value clears");
+   printf("\nall mantiassa bits and increases the exponent value by one:\n");
+   printf("    %s\n", r);
+
+   d=nextafter(d, INFINITY); represent_ieee_754_double(d, r);
+   printf("\nThe increasing of the mantissa value starts over:\n");
+   printf("    %s\n", r);
+
+   d=nextafter(1.0/pow(2, 1021), -INFINITY); represent_ieee_754_double(d, r);
+   printf("\nUntil all mantissa bits are set again:\n");
+   printf("    %s\n", r);
+
+   d=nextafter(d, INFINITY); represent_ieee_754_double(d, r);
+   printf("\nAfter which the mantissa value is reset to 0 and the");
+   printf("\nexponent value is increased by one:\n");
+   printf("    %s\n", r);
+
+}
